89221eec72142a282baf1afbd221e062fc461a55
Victor Lopes <victor.driver@hotmail.com>
2022-03-11 06:57:09 -0300

    refactor: moved wrapper from main to wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:simple_login_app/screens/wrapper.dart';
 import 'package:simple_login_app/services/flutterfire.dart';
-import 'package:simple_login_app/screens/home/home_page.dart';
-import 'package:simple_login_app/screens/login/login_page.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
@@ -47,19 +46,5 @@ class MyApp extends StatelessWidget {
         home: AuthenticationWrapper(),
       ),
     );
-  }
-}
-
-class AuthenticationWrapper extends StatelessWidget {
-  const AuthenticationWrapper({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final firebaseUser = context.watch<User?>();
-
-    if (firebaseUser != null) {
-      return const HomeScreen();
-    }
-    return const LoginScreen();
   }
 }
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:simple_login_app/screens/home/home_page.dart';
+import 'package:simple_login_app/screens/login/login_page.dart';
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return const HomeScreen();
+    }
+    return const LoginScreen();
+  }
+}
