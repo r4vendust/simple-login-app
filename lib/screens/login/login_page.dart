03b2f6c9a07d0bb8a0e1fb5dd3cfccf48cb102b8
r4vendust <victor.driver@hotmail.com>
2024-05-02 01:23:09 -0300

    fix: pixel overflow

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:simple_login_app/services/flutterfire.dart';
 import 'package:simple_login_app/screens/register/register_page.dart';
 import 'package:simple_login_app/utils/colors.dart';
@@ -38,98 +39,105 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 84.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                'lib/assets/flutter_logo.svg',
-                height: 84.0,
-              ),
-              const SizedBox(
-                height: 64.0,
-              ),
-              TextFieldInput(
-                textEditingController: _emailController,
-                icon: Icons.email,
-                hintText: 'Email',
-                labelText: 'Email',
-                textInputType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: 12.0,
-              ),
-              TextFieldInput(
-                textEditingController: _passwordController,
-                icon: Icons.key,
-                hintText: 'Password',
-                labelText: 'Password',
-                textInputType: TextInputType.text,
-                isPass: true,
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              SizedBox(
-                width: 200.0,
-                height: 40.0,
-                child: TextButton(
-                  onPressed: () async {
-                    String isLogged =
-                        await context.read<AuthenticationService>().signIn(
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                            );
-                    if (isLogged == 'Error') {
-                      setOpacity();
-                    }
-                  },
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.blue[500],
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24.0),
-                    ),
-                  ),
-                  child: const Text('Sign in'),
-                ),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Text(
-                'Could not log in. Check your email and password',
-                style: TextStyle(
-                  color: Colors.red.withOpacity(_opacity),
-                ),
-              ),
-              const SizedBox(
-                height: 42.0,
-              ),
-              Row(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text(
-                    "Don't have a account yet?",
+                  SvgPicture.asset(
+                    'lib/assets/flutter_logo.svg',
+                    height: 84.0,
                   ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const RegisterScreen(),
+                  const SizedBox(
+                    height: 64.0,
+                  ),
+                  TextFieldInput(
+                    textEditingController: _emailController,
+                    icon: Icons.email,
+                    hintText: 'Email',
+                    labelText: 'Email',
+                    textInputType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                  TextFieldInput(
+                    textEditingController: _passwordController,
+                    icon: Icons.key,
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    textInputType: TextInputType.text,
+                    isPass: true,
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  SizedBox(
+                    width: 200.0,
+                    height: 40.0,
+                    child: TextButton(
+                      onPressed: () async {
+                        String isLogged =
+                            await context.read<AuthenticationService>().signIn(
+                                  email: _emailController.text,
+                                  password: _passwordController.text,
+                                );
+                        if (isLogged == 'Error') {
+                          setOpacity();
+                        }
+                      },
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue[500],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24.0),
                         ),
-                      );
-                    },
-                    child: const Text('Sign up'),
+                      ),
+                      child: const Text('Sign in'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  Text(
+                    'Could not log in. Check your email and password',
+                    style: TextStyle(
+                      color: Colors.red.withOpacity(_opacity),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 42.0,
+                  ),
+                  FittedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      
+                      children: [
+                        const Text(
+                          "Don't have a account yet?",
+                        ),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const RegisterScreen(),
+                              ),
+                            );
+                          },
+                          child: const Text('Sign up'),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
